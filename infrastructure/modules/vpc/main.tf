e7219c88c71d28857b4bfada9d9b58dbce661dd4
Matthew King <mking38@byu.edu>
2026-09-18 12:21:49 -0600

    Update variables.tf for vpc

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -18,4 +18,24 @@
 #     tags       = { Name = "${var.project}-${var.environment}-vpc" }
 #   }
 
-# TODO: implement the six resources above.
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project}-${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project}-${var.environment}-public-1"
+    Tier = "public"
+  }
+}
